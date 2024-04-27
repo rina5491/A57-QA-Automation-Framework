42840c01a566ca753814e921dcae5d6c047827f4
rina5491 <165849702+rina5491@users.noreply.github.com>
2024-04-27 16:26:35 -0400

    Homework18--

---
+++ src/test/java/HomePage.java
@@ -0,0 +1,11 @@
+public class HomePage {
+    HomePage Homepage= new HomePage (getdriver());
+
+    public Object getDriver() {
+        return driver;
+    }
+
+    public void setDriver(Object driver) {
+        this.driver = driver;
+    }
+}
